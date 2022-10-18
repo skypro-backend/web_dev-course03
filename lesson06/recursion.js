52e3549d41364d73f3ce040ace0253e93f536c97
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-10-18 18:44:11 +0600

    lesson06 video02 time 10:30

--- lesson06/recursion.js
+++ lesson06/recursion.js
@@ -9,6 +9,25 @@ function factorial(number) {
 
 }
 
-debugger;
+// console.log(factorial(5));
 
-console.log(factorial(5));
+/**
+ * F(n) = F(n-1) + F(n-2)
+ * F(0) = 0
+ * F(1) = 1
+ * 0 1 1 2 3 5 8 13
+ */
+function fibonacci(n) {
+
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n === 1) {
+        return 1
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(7));
