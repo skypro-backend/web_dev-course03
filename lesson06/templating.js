71e89da8a315ffc42645bb57322c6e6bbdaa77cc
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-10-18 18:46:17 +0600

    lesson06 video03 time 10:00

--- lesson06/templating.js
+++ lesson06/templating.js
@@ -1,11 +1,16 @@
 const template1 = { tag: 'div' }; // <div></div>
 const template2 = { tag: 'div', content: { tag: 'div' } }; // <div><div></div></div>
 const template3 = undefined;
+const template4 = { tag: 'div', content: 'Привет, шаблон!' }; // <div></div>
 
 function templateEngine(block) {
 
     if ((block === undefined) || (block === null) || (block === false)) {
         return document.createTextNode('');
+    }
+
+    if ((typeof block === 'string') || (typeof block === 'number') || (block === true)) {
+        return document.createTextNode(block);
     }
 
     const element = document.createElement(block.tag);
@@ -18,3 +23,4 @@ const containter = document.querySelector('.container');
 containter.appendChild(templateEngine(template1));
 containter.appendChild(templateEngine(template2));
 containter.appendChild(templateEngine(template3));
+containter.appendChild(templateEngine(template4));
