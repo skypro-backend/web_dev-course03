23f029d246afcd1c7247436e37dba7d84e12699d
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-10-24 18:33:22 +0600

    lesson11 video01 time 11:45

--- lesson11/src/dog.js
+++ lesson11/src/dog.js
@@ -6,31 +6,31 @@
  * возраст (число)
  */
 
-function Dog(name, color, breed, age) {
-    return {
-        name,
-        color,
-        breed,
-        age,
-        bark: function () {
-            console.log('bow-wow');
-        }
-    };
-}
-
-const duffy = Dog('Даффи', 'черный', 'лабрадор', 8);
-
-// class Dog {
-    // constructor(name, color, breed, age) {
-        // this.name = name;
-        // this.color = color;
-        // this.breed = breed;
-        // this.age = age;
-    // }
-
-    // bark() {
-        // console.log('bow-wow');
-    // }
+// function Dog(name, color, breed, age) {
+// return {
+// name,
+// color,
+// breed,
+// age,
+// bark: function () {
+// console.log('bow-wow');
+// }
+// };
 // }
 
-// const duffy = new Dog('Даффи', 'черный', 'лабрадор', 8);
+// const duffy = Dog('Даффи', 'черный', 'лабрадор', 8);
+
+class Dog {
+    constructor(name, color, breed, age) {
+        this.name = name;
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    bark() {
+        console.log('bow-wow');
+    }
+}
+
+const duffy = new Dog('Даффи', 'черный', 'лабрадор', 8);
