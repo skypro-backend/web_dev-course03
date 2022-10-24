91fa552f29e08f765eb7fc208981b1e346b62e35
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-10-24 18:50:23 +0600

    lesson11 video06 time 08:40

--- lesson11/src/inline-edit.js
+++ lesson11/src/inline-edit.js
@@ -7,24 +7,24 @@ class InlineEdit {
         this.element = element;
 
         this.onOriginElementClick = this.onOriginElementClick.bind(this);
-
         this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onFormClick = this.onFormClick.bind(this);
 
         this.element.addEventListener('click', this.onOriginElementClick);
     }
 
     onOriginElementClick() {
-        const currentValue = this.element.textContent;
+        this.currentValue = this.element.textContent;
 
         this.element.innerHTML = '';
 
         this.element.appendChild(templateEngine(
-            InlineEdit.formTemplate(currentValue)
+            InlineEdit.formTemplate(this.currentValue)
         ));
 
         this.element.removeEventListener('click', this.onOriginElementClick);
-
         this.element.addEventListener('submit', this.onFormSubmit);
+        setTimeout(() => this.element.addEventListener('click', this.onFormClick), 0);
     }
 
     onFormSubmit(event) {
@@ -33,10 +33,25 @@ class InlineEdit {
         console.log('submit');
 
         const value = this.element.querySelector('.inline-edit___new-value').value;
+        this.finishEdit(value);
+    }
+
+    onFormClick(event) {
+        const target = event.target;
+
+        if(!target.classList.contains('inline-edit__cancel')) {
+            return;
+        }
+
+        this.finishEdit(this.currentValue);
+    }
+
+    finishEdit(value) {
         this.element.innerHTML = '';
         this.element.textContent = value;
 
-        this.element.addEventListener('click', this.onOriginElementClick);
+        this.element.removeEventListener('click', this.onFormClick);
+        setTimeout(() => this.element.addEventListener('click', this.onOriginElementClick), 0);
         this.element.removeEventListener('submit', this.onFormSubmit);
     }
 }
