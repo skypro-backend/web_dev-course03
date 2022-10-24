979a08d70db7695d591f12bb8188b752ba32d833
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-10-24 18:37:15 +0600

    lesson11 video02 time 10:50

--- lesson11/src/username.js
+++ lesson11/src/username.js
@@ -7,9 +7,19 @@ class Username {
         this.element = element;
         this.guestCheckbox = this.element.querySelector('.username__as-guest-control');
         this.loginInput = this.element.querySelector('.username__login-control');
+
+        this.guestCheckbox.addEventListener('change', this.onCheckboxChange);
     }
 
     getValue() {
         return this.guestCheckbox.checked ? 'Гость' : this.loginInput.value;
+    }
+
+    onCheckboxChange() {
+        if (this.guestCheckbox.checked) {
+            this.loginInput.setAttribute('disabled', 'disabled');
+        } else {
+            this.loginInput.setAttribute('disabled');
+        }
     }
 }
