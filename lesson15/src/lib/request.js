12aad0eb52971c6e27d35c3cac50ab5893fd6e53
Ivan Shchipin <ivan.shchipin@gmail.com>
2022-10-26 19:26:24 +0600

    lesson15 video03 time 03:40

---
+++ lesson15/src/lib/request.js
@@ -0,0 +1,44 @@
+const noop = () => { };
+const NO_PARAMS = {}
+
+function request({
+    method = 'GET',
+    url,
+    params = NO_PARAMS,
+    type = 'json',
+    checkStatusInResponse = false,
+    onSuccess = noop,
+    onError = noop
+}) {
+    const req = new XMLHttpRequest();
+
+    const urlParams = new URLSearchParams(params);
+    const queryString = urlParams.toString();
+
+    req.open(method, url + (queryString ? `?${queryString}` : ''));
+    req.responseType = type;
+
+    req.onload = function (event) {
+        const target = event.target;
+
+        if (target.status !== 200) {
+            onError(target.statusText);
+
+            return;
+        }
+
+        if (checkStatusInResponse && target.response.status !== 'ok') {
+            onError(target.statusText);
+
+            return;
+        }
+
+        onSuccess(target.response);
+    }
+
+    req.onerror = function () {
+        onError();
+    }
+
+    req.send();
+}
